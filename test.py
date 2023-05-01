09a290e24d228af60f81bdce4aa6545a5d8a0712
forellko <dragonforce97q@gmail.com>
2023-05-01 20:59:52 +0300

    Add destructurization

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 # Ask name
 name = input("What is your name? ").title().strip()
 
+# Split user's name into first name and last name
+first, last = name.split()
+
 # Print Hello
-print(f"Hello, {name}" )
+print(f"Hello, {first}" )
