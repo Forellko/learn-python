8e0069bc24c4406c59710bc8765f0b6067c1767c
forellko <dragonforce97q@gmail.com>
2023-05-01 22:14:09 +0300

    test callback it is worked!

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-def hello(to):
+def hello(to, cb):
   print("hello", to)
+  cb()
+  return 
+
+def callback():
+  print('in callback')
 
 name = input("What is your name? ")
 
-hello(name)
+hello(name, callback)
