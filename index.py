a4664492a3cf761356e9d16d066e5238d46c95b3
forellko <dragonforce97q@gmail.com>
2023-05-04 21:42:10 +0300

    csv writter w

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import csv
 
-with open("name.csv") as file:
-    reader = csv.DictReader(file)
-    for read in reader:
-        print(read)
+with open("name.csv", 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['1', '2'])
