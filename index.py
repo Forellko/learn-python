347a9b0a98fab5c57d9ca78d603413fbc508641c
forellko <dragonforce97q@gmail.com>
2023-05-05 17:21:45 +0300

    [a-z]

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import re
 
-email = "dragonforce97q@gmail.com"
+email = 'dragonforce97q@gmail.com'
 
-isValid = re.search(r'^.+@.+/..+$')
+isValid = re.search(r"[a-z]+@[a-z]+\.[a-z]+", email)
 
 if isValid:
     print(1)
