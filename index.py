70ad8e49885db0091731f56c39982c31fc238f9b
forellko <dragonforce97q@gmail.com>
2023-05-07 00:28:43 +0300

    @property @name.setter @classmethod

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
 class Student:
-    def __init__(self, name, house) -> None:
+    def __init__(self, name, house, ) -> None:
         self.name = name
         self.house = house
 
+    @property
+    def name(self):
+        print(1)
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        print(2)
+        self._name = name
+
+    @classmethod
+    def get(cls):
+        return cls()
+
 
 student = Student('Vladimir', 'Taganrog')
+
+student.name = 'Sasha'
 
 print(student.name)
