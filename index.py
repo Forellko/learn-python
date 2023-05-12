b488e85fa699a0afd97c253413dec48d956f2b37
forellko <dragonforce97q@gmail.com>
2023-05-12 20:25:01 +0300

    enumerate

--- index.py
+++ index.py
@@ -4,6 +4,5 @@ students = [
     {"name": 'Roma', 'rate': 7}
 ]
 
-universum = [{student["name"]: "Universum"} for student in students]
-
-print(universum)
+for i, student in enumerate(students):
+    print(i, ' ', student["name"])
